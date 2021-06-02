266fdac1b4516f5fbfb4423c94dc061d0970746b
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-06-02 12:19:27 +0200

    :bookmark: Files, todo and old main vector

--- main.cpp
+++ main.cpp
@@ -1,214 +1,291 @@
-#include "Vector.hpp"
-#include "Iterator.hpp"
+#include <iostream>
+#include "List.hpp"
+#include <vector>
 
-
-void print_intvc(ft::Vector<int> &vc)
+template <typename T>
+void print_list(ft::List<T> &a)
 {
-	std::cout << "Vector contains :";
-	for (size_t i = 0; i < vc.size() ; i++)
-		std::cout << " " << vc[i];
-	std::cout << '\n';
+    std::cout << "List contains :";
+    for (typename ft::List<T>::iterator it = a.begin(); it != a.end(); it++)
+        std::cout << " " << *it;
+    std::cout << '\n';
 }
+
+bool same_integral_part (double first, double second)
+{ return ( int(first)==int(second) ); }
+
+bool inferior_equal_to_two (const int& value) { return (value<=2); }
+
+bool mycomparison (double first, double second)
+{ return ( first < second ); }
+
+//
 
 int main(void)
 {
+    //@constructors, @size, @empty, @max_size
 
-	//@constructors
-	
-	std::cout << "=== CONSTRUCTORS ===" << '\n';
+    std::cout << "=== CONSTRUCTORS, SIZE, EMPTY, MAX_SIZE ===" << '\n';
 
-	std::cout << "\nCase 1: (empty)\n";
-	ft::Vector<int> empty;
-	std::cout << "-> Size = " << empty.size() << '\n';
-	std::cout << "-> Capacity = " << empty.capacity() << '\n';
-	std::cout << "-> Is empty = " << empty.empty() << '\n';
-	print_intvc(empty);
+    //empty
+    ft::List<int> a;
+    std::cout << "-> Size = " << a.size() << '\n';
+    std::cout << "-> Empty = " << a.empty() << '\n';
+    std::cout << "-> Max_size = " << a.max_size() << '\n';
+    print_list(a);
 
-	std::cout << "\nCase 2: (7, 5)\n";
-	ft::Vector<int> one(7, 5);
-	std::cout << "-> Size = " << one.size() << '\n';
-	std::cout << "-> Capacity = " << one.capacity() << '\n';
-	std::cout << "-> Is empty = " << one.empty() << '\n';
-	print_intvc(one);
+    //values
+    ft::List<int> b(7, 42);
+    std::cout << "\n-> Size = " << b.size() << '\n';
+    std::cout << "-> Empty = " << b.empty() << '\n';
+    std::cout << "-> Max_size = " << b.max_size() << '\n';
+    print_list(b);
 
-	std::cout << "\nCase 3: (begin, end) of last vector\n";
-	ft::Vector<int> two(one.begin(), one.end());
-	std::cout << "-> Size = " << two.size() << '\n';
-	std::cout << "-> Capacity = " << two.capacity() << '\n';
-	std::cout << "-> Is empty = " << two.empty() << '\n';
-	print_intvc(two);
+    //first last iterators
+    ft::List<int> c(b.begin(), b.end());
+    std::cout << "\n-> Size = " << c.size() << '\n';
+    std::cout << "-> Empty = " << c.empty() << '\n';
+    std::cout << "-> Max_size = " << c.max_size() << '\n';
+    print_list(c);
 
-	
-	//@iterator , @begin , @end, @push_back
+    //copy
+    ft::List<int> d(c);
+    std::cout << "\n-> Size = " << d.size() << '\n';
+    std::cout << "-> Empty = " << d.empty() << '\n';
+    std::cout << "-> Max_size = " << d.max_size() << '\n';
+    print_list(d);
+    
+    //@assign
 
-	std::cout << "\n=== ITERATOR, BEGIN, END ===\n\n";
-	ft::Vector<int> test1;
+    std::cout << "\n=== ASSIGN ===" << '\n';
 
-	test1.push_back(45);
-	test1.push_back(47);
-	test1.push_back(41);
-	test1.push_back(666);
-	test1.push_back(667);
-	print_intvc(test1);
+    a.assign(12, 100);
+    print_list(a);
+    std::cout << "-> Size = " << a.size() << '\n';
 
-	//@at
+    b.assign(a.begin(), a.begin() + 2);
+    print_list(b);
+    std::cout << "-> Size = " << b.size() << '\n';
 
-	std::cout << "=== AT ===" << '\n';
-	std::cout << "* Test out of range! *\n";
-	ft::Vector<int> test2(10);
+    //@back, @front, @push_back, @pop_back, @push_front, @pop_front
 
-	for (unsigned i = 0; i< test2.size(); i++)
-		test2.at(i) = i;
-	try
-	{
-		std::cout << test2.at(42) << '\n';
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	print_intvc(test2);
+    std::cout << "\n=== BACK, FRONT, PUSH_BACK, POP_BACK , PUSH_FRONT, POP_FRONT  ===" << '\n';
 
-	//@front, @back
+    ft::List<char> e;
 
-	std::cout << "\n=== FRONT, BACK ===\n\n";
-	std::cout << "-> Front = " << test2.front() << '\n';
-	std::cout << "-> Back = " << test2.back() << '\n';
+    e.push_back('x');
+    e.push_back('$');
+    e.push_back('k');
+    e.push_back('r');
+    e.push_back('a');
+    e.push_back('T');
+    std::cout << "\n-> Front = " << e.front() << '\n';
+    std::cout << "-> Back = " << e.back() << '\n';
+    print_list(e);
 
-	//@capacity
+    std::cout << "\n * pop_back X3 *\n";
+    e.pop_back();
+    e.pop_back();
+    e.pop_back();
+    std::cout << "\n-> Front = " << e.front() << '\n';
+    std::cout << "-> Back = " << e.back() << '\n';
+    print_list(e);
 
-	std::cout << "\n=== CAPACITY ===\n\n";
-	std::cout << "-> Capacity (vect1) = " << test1.capacity() << '\n';
-	std::cout << "-> Capacity (vect2) = " << test2.capacity() << '\n';
+    std::cout << "\n* push_front (U S N U : :) *\n";
+    std::cout << "* pop_front X2 *\n";
 
-	//@clear, @empty
+    e.push_front('U');
+    e.push_front('S');
+    e.push_front('N');
+    e.push_front('U');
+    e.push_front(':');
+    e.push_front(':');
+    e.pop_front();
+    e.pop_front();
+    print_list(e);
 
-	std::cout << "\n=== CLEAR, EMPTY ===\n\n";
-	std::cout << "-> Empty = " << test2.empty() << '\n';
-	test2.clear();
-	std::cout << "-> Empty = " << test2.empty() << '\n';
-	print_intvc(test2);
+    //@begin, @end, @rbegin, @rend
 
-	//@erase
+    std::cout << "\n=== BEGIN, END, RBEGIN, REND ===\n\n";
 
-	std::cout << "\n=== ERASE ===\n\n";
-	print_intvc(test1);
-	std::cout << "* Erase! *\n";
-	test1.erase(test1.begin(), test1.begin() + 2);
-	print_intvc(test1);
+    std::cout << "List contains (normal) :";
+    for (ft::List<char>::iterator it = e.begin(); it != e.end(); it++)
+        std::cout << " " << *it;
+    std::cout << '\n';
 
-	//@insert (3 ways)
+    std::cout << "List contains (reverse) :";
+    for (ft::List<char>::reverse_iterator it = e.rbegin(); it != e.rend(); it++)
+        std::cout << " " << *it;
+    std::cout << '\n';
 
-	std::cout << "\n=== INSERT ===\n\n";
-	ft::Vector<int> myvector(3, 100);
-	std::cout << "* New vector contains 3 values of 100 *\n";
-	print_intvc(myvector);
-	ft::Vector<int>::iterator it;
-	it = myvector.begin();
-	std::cout << "* Insert from the begin, the value 200 *\n";
-	it = myvector.insert(it, 200);
-	print_intvc(myvector);
-	it = myvector.begin();
-	std::cout << "* Insert from the begin, 2 values of 300 *\n";
-	myvector.insert(it, 2, 300); //@1
-	print_intvc(myvector);
-	it = myvector.begin();
-	ft::Vector<int> anothervector(2, 400);
-	std::cout << "* Creating another vector that contains 2 values of 400 *\n";
-	std::cout << "* Insert from the begin+2, the value begin, to end of the another vector *\n";
-	myvector.insert(it + 2, anothervector.begin(), anothervector.end()); //@2
-	print_intvc(myvector);
-	int myarray [] = {501, 502, 503};
-	std::cout << "* Creating an array of integers, values {501, 502, 503} *\n";
-	std::cout << "* Insert from the begin, the first value of the array, until the first+3th value *\n";
-	myvector.insert(myvector.begin(), myarray, myarray + 3);
-	print_intvc(myvector);
+    //@erase, @resize, @clear
+    
+    std::cout << "\n=== ERASE, RESIZE, CLEAR ===\n\n";
 
-	//@rbegin, @rend
+    print_list(b);
+    std::cout << "* Resize (15) ! *\n";
+    b.resize(15);
+    print_list(b);
+    std::cout << "* Erase (begin+1, end) ! *\n";
+    b.erase(b.begin() + 1, b.end());
+    print_list(b);
+    std::cout << "* Clear ! *\n";
+    b.clear();
+    print_list(b);
 
-	std::cout << "\n=== RBEGIN, REND ===\n\n";
-	std::cout << "-> Iteration reverse on the vector :";
-	for (ft::Vector<int>::reverse_iterator rit = myvector.rbegin(); rit != myvector.rend(); rit++)
-		std::cout << " " << *rit;
-	std::cout << '\n';
+    //@insert
 
-	//@max_size
+    std::cout << "\n=== INSERT ===\n\n";
 
-	std::cout << "\n=== MAX_SIZE ===\n\n";
-	std::cout << "-> Max size = " << myvector.max_size() << '\n';
-	std::cout << "-> Max size = " << test1.max_size() << '\n';
-	std::cout << "-> Max size = " << test2.max_size() << '\n';
+    ft::List<int> mylist;
+    ft::List<int>::iterator it;
 
-	//@operator=, @operator[], @push_back, @pop_back
+    for (int i = 1; i <= 5; ++i)
+        mylist.push_back(i);
 
-	std::cout << "\n=== OPERATOR '=', '[]', POP/PUSH_BACK ===\n\n";
+    it = mylist.begin();
 
-	ft::Vector<int> Foo(4, 17);
-	ft::Vector<int> Fuu;
-	ft::Vector<int> Bar(6, 67);
+    mylist.insert(it,10);
+    mylist.insert(it,2,20);
 
-	Foo = Bar;
-	Fuu = Foo;
-	print_intvc(Foo);
-	print_intvc(Fuu);
-	std::cout << "* Push_back (667) *\n";
-	Fuu.push_back(667);
-	print_intvc(Fuu);
-	std::cout << "* Pop_back X3 *\n";
-	Fuu.pop_back();
-	Fuu.pop_back();
-	Fuu.pop_back();
-	print_intvc(Fuu);
-	Fuu[0] = -666;
-	Fuu[2] = -667;
-	print_intvc(Fuu);
+    --it;
+    std::vector<int> myvector (2,30);
+    mylist.insert (it,myvector.begin(),myvector.end());
+    print_list(mylist);
 
-	//@capacity, @reserve, @resize
+    //@merge, @sort
 
-	std::cout << "\n=== RESERVE, RESIZE ===\n\n";
-	ft::Vector<int> bigcap(20);
-	print_intvc(bigcap);
-	std::cout << "-> Capacity = " << bigcap.capacity() << '\n';
-	std::cout << "* Reserve! (4242) *\n";
-	bigcap.reserve(4242);
-	std::cout << "-> Capacity = " << bigcap.capacity() << '\n';
-	print_intvc(bigcap);
-	std::cout << "* Resize ! (30) *\n";
-	bigcap.resize(30);
-	std::cout << "-> Size = " << bigcap.size() << '\n';
-	print_intvc(bigcap);
-	std::cout << "* Resize ! (5) *\n";
-	bigcap.resize(5);
-	std::cout << "-> Size = " << bigcap.size() << '\n';
-	print_intvc(bigcap);
+    std::cout << "\n=== MERGE, SORT ===\n\n";
 
-	//@swap
+    ft::List<double> first, second;
 
-	std::cout << "\n=== SWAP ===\n\n";
+    first.push_back (3.1);
+    first.push_back (2.2);
+    first.push_back (2.9);
+
+    second.push_back (3.7);
+    second.push_back (7.1);
+    second.push_back (1.4);
+
+    std::cout << "\n* Sort each list *\n";
+    first.sort();
+    second.sort();
+    print_list(first);
+    print_list(second);
+
+    std::cout << "\n* Merge *\n";
+    first.merge(second);
+    print_list(first);
+
+
+    //@remove_if , @remove
+
+    std::cout << "\n=== REMOVE, REMOVE_IF ===\n\n";
+
+    print_list(first);
+    std::cout << "\n* Remove value (3.1) *\n\n";
+    first.remove(3.1);
+    print_list(first);
+
+    std::cout << "\n* Remove_if(<=2) *\n\n";
+    first.remove_if(inferior_equal_to_two);
+    print_list(first);
+
+    //@reverse
+
+    std::cout << "\n=== REVERSE ===\n\n";
+
+
+    first.push_back(42);
+    first.push_back(424);
+    first.push_back(429);
+    first.push_back(4277);
+
+    print_list(first);
+    std::cout << "* Reverse *\n";
+    first.reverse();
+    print_list(first);
+
+    //@splice
+
+    std::cout << "\n=== SPLICE ===\n\n";
+    ft::List<int> mylist1, mylist2;
+    ft::List<int>::iterator iteer;
+
+    for (int i = 1; i <= 4; ++i)
+        mylist1.push_back(i);
+
+    for (int i = 1; i <= 3; ++i)
+        mylist2.push_back(i * 10);
+
+    iteer = mylist1.begin();
+
+    mylist1.splice(iteer, mylist2);                              
+    mylist2.splice (mylist2.begin(),mylist1, iteer);
+    iteer = mylist1.begin();
+    iteer += 3;
+    mylist1.splice ( mylist1.begin(), mylist1, iteer, mylist1.end());
+    print_list(mylist1);
+    print_list(mylist2);
+
+    //@swap
+
+    std::cout << "\n=== SWAP ===\n\n";
 
 	std::cout << "\n1) ";
-	print_intvc(myvector);
-	std::cout << "   Size = " << myvector.size() << '\n';
-	std::cout << "   Capacity = " << myvector.capacity() << '\n';
+	print_list(a);
+	std::cout << "   Size = " << a.size() << '\n';
 	std::cout << "\n2) ";
-	print_intvc(test1);
-	std::cout << "   Size = " << test1.size() << '\n';
-	std::cout << "   Capacity = " << test1.capacity() << '\n';
-	ft::Vector<int>::iterator itera = myvector.begin();
+	print_list(c);
+	std::cout << "   Size = " << c.size() << '\n';
+	ft::List<int>::iterator itera = a.begin();
 	std::cout << "   Pointer before swap = " << *itera << '\n';
 
 	std::cout << "\n* Swap! *\n\n";
-	myvector.swap(test1);
+	a.swap(c);
 
 
 	std::cout << "\n1) ";
-	print_intvc(myvector);
-	std::cout << "   Size = " << myvector.size() << '\n';
-	std::cout << "   Capacity = " << myvector.capacity() << '\n';
+	print_list(a);
+	std::cout << "   Size = " << a.size() << '\n';
 	std::cout << "\n2) ";
-	print_intvc(test1);
-	std::cout << "   Size = " << test1.size() << '\n';
-	std::cout << "   Capacity = " << test1.capacity() << '\n';
+	print_list(c);
+	std::cout << "   Size = " << c.size() << '\n';
 	std::cout << "   Pointer after swap = " << *itera << '\n';
-	return (0);
+
+    //@operator = 
+
+    std::cout << "\n=== OPERATOR '=' ===\n\n";
+
+    ft::List<int> primero (3);
+    ft::List<int> secundo (5);
+
+    secundo = primero;
+    primero = ft::List<int>();
+
+    std::cout << "Size of primero: " << int (primero.size()) << '\n';
+    std::cout << "Size of secundo: " << int (secundo.size()) << '\n';
+
+    //@unique
+
+    std::cout << "\n=== UNIQUE '=' ===\n\n";
+
+    double mydoubles[] = {12.15, 2.72, 73.0, 12.77, 3.14, 12.77, 73.35, 72.25, 15.3, 72.25};
+
+    ft::List<double> unique_list (mydoubles,mydoubles+10);
+    unique_list.sort();
+
+
+    print_list(unique_list);
+    unique_list.unique();
+
+    std::cout << "* Simple unique *\n";
+    unique_list.unique();
+    print_list(unique_list);
+    
+    std::cout << "* Unique function *\n";
+    unique_list.unique (same_integral_part);
+    print_list(unique_list);
+
+    return 0;
 }
