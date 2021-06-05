64b5b2829bec7fad34e5d659948268c097b40bf5
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-06-05 09:43:37 +0200

    :package: Preparing map main, and header template file

--- headers/Map.hpp
+++ headers/Map.hpp
@@ -42,11 +42,13 @@ namespace ft
 						return comp(x.first, y.first);
 					};
 			};
+
 		private:
 			allocator_type _allocator;
 			key_compare _comp;
 			node _root;
 			size_type _length;
+
 			void _debug_tree(node n)
 			{
 				if (!n)
@@ -55,7 +57,8 @@ namespace ft
 				if (n->parent && !n->end)
 					std::cout << n->pair.first << "=" << n->pair.second << std::endl;
 				_debug_tree(n->right);
-			};
+			}
+
 			node _new_node(key_type key, mapped_type value, node parent, bool end = false)
 			{
 				node el = new BNode<key_type, mapped_type>();
@@ -65,7 +68,8 @@ namespace ft
 				el->parent = parent;
 				el->end = end;
 				return (el);
-			};
+			}
+
 			void _free_tree(node n)
 			{
 				if (n->right)
@@ -73,7 +77,8 @@ namespace ft
 				if (n->left)
 					_free_tree(n->left);
 				delete n;
-			};
+			}
+
 			node _insert_node(node n, key_type key, mapped_type value, bool end = false)
 			{
 				if (n->end)
@@ -105,7 +110,8 @@ namespace ft
 					else
 						return(_insert_node(n->right, key, value));
 				}
-			};
+			}
+
 			node _find(node n, key_type key) const
 			{
 				node tmp;
@@ -122,7 +128,8 @@ namespace ft
 						return (tmp);
 				}
 				return (0);
-			};
+			}
+
 			void _delete_node(node n)
 			{
 				node parent = n->parent;
@@ -160,45 +167,52 @@ namespace ft
 					next = (--iterator(n)).node();
 				ft::swap(next->pair, n->pair);
 				_delete_node(next);
-			};
+			}
+
 			void _init_tree(void)
 			{
 				_root = _new_node(key_type(), mapped_type(), 0);
 				_root->right  = _new_node(key_type(), mapped_type(), _root, true);
 				_length = 0;
-			};
+			}
 			node _end(void) const
 			{
 				return (_root->right);
-			};
+			}
+
 		public:
 			explicit Map(const key_compare &comp = key_compare(), const allocator_type alloc = allocator_type())
 			: _allocator(alloc), _comp(comp)
 			{
 				_init_tree();
-			};
+			}
+
 			template <class InputIterator>
 			Map(InputIterator first, InputIterator last, const key_compare &comp = key_compare(), const allocator_type alloc = allocator_type())
 			: _allocator(alloc), _comp(comp)
 			{
 				_init_tree();
 				insert(first, last);
-			};
+			}
+
 			Map(const Map<Key, T> &other)
 			{
 				_init_tree();
 				*this = other;
-			};
+			}
+
 			~Map(void)
 			{
 				_free_tree(_root);	
-			};
+			}
+
 			Map &operator=(const Map<Key, T> &other)
 			{
 				clear();
 				insert(other.begin(), other.end());
 				return (*this);
-			};
+			}
+
 			iterator begin(void)
 			{
 				node n = _root;
@@ -209,7 +223,8 @@ namespace ft
 				while (n->left)
 					n = n->left;
 				return (iterator(n));
-			};
+			}
+
 			const_iterator begin(void) const
 			{
 				node n = _root;
@@ -220,47 +235,57 @@ namespace ft
 				while (n->left)
 					n = n->left;
 				return (const_iterator(n));
-			};
+			}
+
 			iterator end(void)
 			{
 				return (iterator(_end()));
-			};
+			}
+
 			const_iterator end(void) const
 			{
 				return (const_iterator(_end()));
-			};
+			}
+
 			reverse_iterator rbegin(void)
 			{
 				iterator i = end();
 				i--;
 				return (reverse_iterator(i.node()));
-			};
+			}
+
 			const_reverse_iterator rbegin(void) const
 			{
 				iterator i = end();
 				i--;
 				return (const_reverse_iterator(i.node()));
-			};
+			}
+
 			reverse_iterator rend(void)
 			{
 				return (reverse_iterator(_root));
-			};
+			}
+
 			const_reverse_iterator rend(void) const
 			{
 				return (const_reverse_iterator(_root));
-			};
+			}
+
 			bool empty(void) const
 			{
 				return (_length == 0);
-			};
+			}
+
 			size_type size(void) const
 			{
 				return (_length);
-			};
+			}
+
 			size_type max_size(void) const
 			{
 				return (std::numeric_limits<size_type>::max() / (sizeof(ft::BNode<key_type, mapped_type>)));
-			};
+			}
+
 			mapped_type &operator[](const key_type& k)
 			{
 				iterator tmp = find(k);
@@ -269,7 +294,8 @@ namespace ft
 					return tmp->second;
 				}
 				return (insert(std::make_pair(k, mapped_type())).first->second);
-			};
+			}
+
 			std::pair<iterator, bool> insert(const value_type &value)
 			{
 				iterator tmp;
@@ -277,7 +303,8 @@ namespace ft
 					return (std::make_pair(tmp, false));
 				++_length;
 				return (std::make_pair(iterator(_insert_node(_root, value.first, value.second)), true));
-			};
+			}
+
 			iterator insert(iterator position, const value_type &value)
 			{
 				iterator tmp;
@@ -285,7 +312,8 @@ namespace ft
 					return (tmp);
 				++_length;
 				return (iterator(_insert_node(position.node(), value.first, value.second)));
-			};
+			}
+
 			template <class InputIterator>
 			void insert(InputIterator first, InputIterator last)
 			{
@@ -294,7 +322,8 @@ namespace ft
 					insert(*first);
 					++first;
 				}
-			};
+			}
+
 			void erase(iterator position)
 			{
 				_delete_node(position.node());
@@ -310,29 +339,35 @@ namespace ft
 					++i;
 				};
 				return (i);
-			};
+			}
+		
 			void erase(iterator first, iterator last)
 			{
 				while (first != last)
 					erase(first++);
-			};
+			}
+
 			void swap(Map &x)
 			{
 				ft::swap(_length, x._length);
 				ft::swap(_root, x._root);
-			};
+			}
+
 			void clear(void)
 			{
 				erase(begin(), end());
-			};
+			}
+
 			key_compare key_comp(void) const
 			{
 				return (_comp);
-			};
+			}
+
 			value_compare value_comp(void) const
 			{
 				return (this->value_compare);
-			};
+			}
+
 			iterator find(const key_type &value)
 			{
 				if (empty())
@@ -341,7 +376,8 @@ namespace ft
 				if (tmp)
 					return (iterator(tmp));
 				return (end());
-			};
+			}
+
 			const_iterator find(const key_type &value) const
 			{
 				if (empty())
@@ -350,7 +386,8 @@ namespace ft
 				if (tmp)
 					return (const_iterator(tmp));
 				return (end());
-			};
+			}
+
 			size_type count(const key_type &value) const
 			{
 				size_t c = 0;
@@ -363,7 +400,8 @@ namespace ft
 					++it;
 				}
 				return (c);
-			};
+			}
+
 			iterator lower_bound(const key_type &key)
 			{
 				iterator it = begin();
@@ -374,7 +412,8 @@ namespace ft
 					++it;
 				}
 				return (end());
-			};
+			}
+
 			const_iterator lower_bound(const key_type &key) const
 			{
 				const_iterator it = begin();
@@ -385,7 +424,8 @@ namespace ft
 					++it;
 				}
 				return (end());
-			};
+			}
+
 			iterator upper_bound(const key_type &key)
 			{
 				iterator it = begin();
@@ -396,7 +436,8 @@ namespace ft
 					++it;
 				};
 				return (end());
-			};
+			}
+
 			const_iterator upper_bound(const key_type &key) const
 			{
 				const_iterator it = begin();
@@ -407,19 +448,17 @@ namespace ft
 					++it;
 				};
 				return (end());
-			};
+			}
+
 			std::pair<const_iterator, const_iterator> equal_range(const key_type &k) const
 			{
 				return (std::pair<const_iterator, const_iterator>(this->lower_bound(k), this->upper_bound(k)));
-			};
+			}
+
 			std::pair<iterator, iterator> equal_range(const key_type &k)
 			{
 				return (std::pair<iterator, iterator>(this->lower_bound(k), this->upper_bound(k)));
-			};
-			// void debug(void)
-			// {
-			// 	_debug_tree(_root);
-			// };
+			}
 	};
 	template <class Key, class T, class Compare, class Alloc>
 	void swap(ft::Map<Key, T, Compare, Alloc> &x, ft::Map<Key, T, Compare, Alloc> &y)
